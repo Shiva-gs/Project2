e63ef84488933cc67e31f7eb3f474a25fac84ca8
DorienFasolati <34323192+DorienFasolati@users.noreply.github.com>
2019-11-19 21:09:54 -0500

    adding templates and js

---
+++ Travel_finderz/static/js/config.js
@@ -0,0 +1,2 @@
+// API key
+const API_KEY = "pk.eyJ1IjoiamFtYm9uZzQ3IiwiYSI6ImNrMzZseWs0MjAzNHYzZG52Y3lybmZ3dmsifQ.2F1jTOE2UXpF1-E4gwpFeQ";
---
+++ Travel_finderz/static/js/logic.js
@@ -0,0 +1,48 @@
+// Create a map object
+var myMap = L.map("map", {
+  center: [37.09, -95.71],
+  zoom: 5
+});
+
+// Add a tile layer
+L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+  maxZoom: 18,
+  id: "mapbox.streets",
+  accessToken: API_KEY
+}).addTo(myMap);
+
+// An array containing each city's name, location, and population
+var cities = [{
+  location: [40.7128, -74.0059],
+  name: "New York",
+},
+{
+  location: [13.756331, 100.501762],
+  name: "Bangkok",
+},
+{
+  location: [51.507351, -0.127758],
+  name: "London",
+},
+{
+  location: [25.204849, 55.270782],
+  name: "Dubai",
+},
+{
+  location: [1.3521, 103.8198],
+  name: "Singapore ",
+},
+{
+  location: [48.8566, 2.3522],
+  name: "Paris ",
+}
+];
+
+// Loop through the cities array and create one marker for each city, bind a popup containing its name and population add it to the map
+for (var i = 0; i < cities.length; i++) {
+  var city = cities[i];
+  L.marker(city.location)
+    .bindPopup("<h1>" + city.name + "</h1> <hr>")
+    .addTo(myMap);
+}
