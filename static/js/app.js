d375b9b545b4f8d7783b47a1a4bc15ae13791ca3
Shiva-gs <53023712+Shiva-gs@users.noreply.github.com>
2019-11-23 11:26:52 -0500

    Add files via upload

---
+++ static/js/app.js
@@ -0,0 +1,28 @@
+function init() {
+  // Grab a reference to the dropdown select element
+  var selector = d3.select("#selDataset");
+
+  // Use the list of sample location to populate the select options
+  d3.json("/locations").then((sampleNames) => {
+    sampleNames.forEach((sample) => {
+      selector
+        .append("option")
+        .text(sample)
+        .property("value", sample);
+    });
+
+    // Use the first sample from the list to build the initial plots
+    const firstSample = sampleNames[0];
+    buildCharts(firstSample);
+    buildMetadata(firstSample);
+  });
+}
+
+function optionChanged(newSample) {
+  // Fetch new data each time a new sample is selected
+  buildCharts(newSample);
+  buildMetadata(newSample);
+}
+
+// Initialize the dashboard
+init();
