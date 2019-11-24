9d4c258bb4bf6a25f3cbdbf39a2db48db25a23c2
Shiva-gs <53023712+Shiva-gs@users.noreply.github.com>
2019-11-23 22:03:06 -0500

    Update app.js

--- static/js/app.js
+++ static/js/app.js
@@ -1,6 +1,11 @@
 function init() {
+  console.log("in init");
+  
+   
   // Grab a reference to the dropdown select element
   var selector = d3.select("#selDataset");
+  var cat_selector = d3.select("#selCategory");
+  var ds_selector = d3.select("#selDataSource");
 
   // Use the list of sample location to populate the select options
   d3.json("/locations").then((sampleNames) => {
@@ -11,18 +16,72 @@ function init() {
         .property("value", sample);
     });
 
-    // Use the first sample from the list to build the initial plots
-    const firstSample = sampleNames[0];
-    buildCharts(firstSample);
-    buildMetadata(firstSample);
+
+  });
+
+    // ..  Use the list of sample category to populate the select options
+    d3.json("/category").then((sampleNames) => {
+      sampleNames.forEach((sample) => {
+        console.log("sample"+sample);
+        cat_selector
+          .append("option")
+          .text(sample)
+          .property("value", sample);
+      });
+  
+    });
+
+
+    
+}
+function optionChanged(newSample) {
+  // Fetch new data each time a new sample is selected
+  //buildCharts(newSample);
+  //buildMetadata(newSample);
+  //console.log(newSample);
+
+
+
+var e = document.getElementById("selCategory");
+
+var category = e.options[e.selectedIndex].text;
+console.log(category);
+
+console.log("/citydata/"+newSample+","+ category);
+  d3.json("/citydata/"+newSample+","+category).then((sampleNames) => {
+    sampleNames.forEach((sample) => {
+      console.log(sample);
+    });
   });
 }
 
-function optionChanged(newSample) {
+function optionChanged1(newSample) {
   // Fetch new data each time a new sample is selected
-  buildCharts(newSample);
-  buildMetadata(newSample);
+  //buildCharts(newSample);
+  //buildMetadata(newSample);
+  //console.log(newSample);
+
+
+
+var e = document.getElementById("selDataset");
+
+var location = e.options[e.selectedIndex].text;
+console.log(location);
+
+console.log("/citydata/"+location+","+ newSample);
+  d3.json("/citydata/"+location+","+newSample).then((sampleNames) => {
+    sampleNames.forEach((sample) => {
+      console.log(sample);
+    });
+
+    // Use the first sample from the list to build the initial plots
+    //const firstSample = sampleNames[0];
+    //buildCharts(firstSample);
+    //buildMetadata(firstSample);
+  });
+  
 }
+
 
 // Initialize the dashboard
 init();
