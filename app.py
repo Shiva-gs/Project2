84bb38ca12244d10bc43d23b0f922bbd62d2c9dd
Shiva-gs <53023712+Shiva-gs@users.noreply.github.com>
2019-11-23 22:10:36 -0500

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import pymongo
 import json
 import os
 from flask import Flask, jsonify, render_template
+import getCityData
 
 
 # create instance of Flask app
@@ -19,6 +20,8 @@ db = travel.traveldata
 locations = db.location.find() 
 loc_df = pd.DataFrame(list(locations))
 
+targets = db.target_type.find() 
+target_df = pd.DataFrame(list(targets))
 
 # create route that renders index.html template
 @app.route("/")
@@ -32,6 +35,18 @@ def locations():
 
     # Return a list of the column location (locations)
     return jsonify(list(loc_df['name']))
+
+@app.route("/category")
+def category():
+    """Return a list of Target type."""
+    # Return a list of the column category  
+    return jsonify(list(target_df['target_type']))
+
+@app.route("/citydata/<location>,<targe_type>")
+def citydata(location,targe_type):
+    city_data = getCityData.get_data(location,targe_type)
+    df= city_data.drop(['_id'], axis=1)
+    return df.to_json(orient='records') 
 
 if __name__ == "__main__":
     app.run(debug=True)
