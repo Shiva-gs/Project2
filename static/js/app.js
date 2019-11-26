dda03bfbae46249b68dca4f8adfde6c604b9e868
Shiva-gs <53023712+Shiva-gs@users.noreply.github.com>
2019-11-26 17:01:42 -0500

    Update app.js

--- static/js/app.js
+++ static/js/app.js
@@ -1,3 +1,45 @@
+function buildMap(sample)
+{
+  // clean the previous markings
+  //map.off();
+  //map.remove();
+  var container = L.DomUtil.get('map');
+      if(container != null){
+        container._leaflet_id = null;
+      }
+
+  var map = L.map("map", {
+    center: [40.7657325, -73.9692262],
+    zoom: 13
+  });
+
+  L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+  maxZoom: 18,
+  id: "mapbox.streets"
+}).addTo(map);
+
+// .bindPopup("<h1>" + "New York" + "</h1> <hr> <h3>Restaurant " + "The Lowell Hotel" + "</h3>")
+L.marker([40.7657325, -73.9692262])
+  .bindPopup("<h1>" + "New York" + "</h1> <hr> <h3>Restaurant " + "The Lowell Hotel" + "</h3>")
+  .addTo(map);
+  
+  console.log("Printed the map")
+  console.log(Object.keys(sample).length);
+  //sampleNames.forEach((sample) => {
+    //console.log(sample);
+    console.log(sample) 
+  //}
+  //for (var i = 0; i < 6; i++) {
+   
+   // var city = cities[i];
+   // L.marker(city.location)
+   //   .bindPopup("<h1>" + city.name + "</h1> <hr> <h3>Population " + city.population + "</h3>")
+   //   .addTo(myMap);
+  //}
+
+}
+
 function init() {
   console.log("in init");
   
@@ -68,19 +110,38 @@ var e = document.getElementById("selDataset");
 var location = e.options[e.selectedIndex].text;
 console.log(location);
 
-console.log("/citydata/"+location+","+ newSample);
+//console.log("/citydata/"+location+","+ newSample);
   d3.json("/citydata/"+location+","+newSample).then((sampleNames) => {
+
     sampleNames.forEach((sample) => {
       console.log(sample);
+      //console.log(sample.geometry.location.lat)
+      //console.log(sample.geometry.location.lng) 
     });
-
     // Use the first sample from the list to build the initial plots
     //const firstSample = sampleNames[0];
     //buildCharts(firstSample);
     //buildMetadata(firstSample);
+    //console.log("Calling Map refresh ");
+    buildMap(sampleNames);
   });
   
+
+
 }
+ 
+// Creating map object
+var map = L.map("map", {
+  center: [0, 0],
+  zoom: 1
+});
+
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+  maxZoom: 18,
+  id: "mapbox.streets"
+}).addTo(map);
+
 
 
 // Initialize the dashboard
