dcd362db165e9cea340e21e98268ccbf725c4261
DorienFasolati <34323192+DorienFasolati@users.noreply.github.com>
2019-11-30 14:58:08 -0500

    Add files via upload

---
+++ Travel_finderz/static/js/config.js
@@ -0,0 +1,1 @@
+const API_KEY = "pk.eyJ1IjoiamFtYm9uZzQ3IiwiYSI6ImNrMzZseWs0MjAzNHYzZG52Y3lybmZ3dmsifQ.2F1jTOE2UXpF1-E4gwpFeQ";
--- Travel_finderz/static/js/logic.js
+++ Travel_finderz/static/js/logic.js
@@ -13,36 +13,60 @@ L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
 }).addTo(myMap);
 
 // An array containing each city's name, location, and population
-var cities = [{
-  location: [40.7128, -74.0059],
-  name: "New York",
-},
-{
-  location: [13.756331, 100.501762],
-  name: "Bangkok",
-},
-{
-  location: [51.507351, -0.127758],
-  name: "London",
-},
-{
-  location: [25.204849, 55.270782],
-  name: "Dubai",
-},
-{
-  location: [1.3521, 103.8198],
-  name: "Singapore ",
-},
-{
-  location: [48.8566, 2.3522],
-  name: "Paris ",
-}
-];
+// var cities = [{
+//   location: [40.7128, -74.0059],
+//   name: "New York",
+//   video: "http://127.0.0.1:5000/newyork"
+// },
+// {
+//   location: [13.756331, 100.501762],
+//   name: "Bangkok",
+// },
+// {
+//   location: [51.507351, -0.127758],
+//   name: "London",
+// },
+// {
+//   location: [25.204849, 55.270782],
+//   name: "Dubai",
+// },
+// {
+//   location: [1.3521, 103.8198],
+//   name: "Singapore ",
+// },
+// {
+//   location: [48.8566, 2.3522],
+//   name: "Paris ",
+// }
+// ];
 
-// Loop through the cities array and create one marker for each city, bind a popup containing its name and population add it to the map
-for (var i = 0; i < cities.length; i++) {
-  var city = cities[i];
-  L.marker(city.location)
-    .bindPopup("<h1>" + city.name + "</h1> <hr>")
-    .addTo(myMap);
+// // Loop through the cities array and create one marker for each city, bind a popup containing its name and population add it to the map
+// for (var i = 0; i < cities.length; i++) {
+//   var city = cities[i];
+//   L.marker(city.location)
+//     .bindPopup("<h1>" + city.name + "</h1> <hr>")
+//     .addTo(myMap);
+// }
+
+
+// function add_marker() {
+  var points = 
+      ["New York", 40.7128, -74.0059, "http://127.0.0.1:5000/newyork"]
+ 
+  // ];
+  
+  var marker = [];
+  var i;
+  for (i = 0; i < points.length; i++) {
+      marker[i] = new L.Marker([points[i][1], points[i][2]], {
+          win_url: points[i][3]
+      });
+      marker[i].addTo(myMap);
+      marker[i].on('click', onClick);
+  };
+}
+
+function onClick(e) {
+  //console.log(this.options.win_url);
+  window.open(this.options.win_url);
 }
